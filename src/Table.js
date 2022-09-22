4e419f6d38b7b00bfdc6d82c1fd9989c6bea54c1
marinm <marinmema@live.ca>
2022-09-21 23:01:58 -0400

    Refactor classes as functions

--- src/Table.js
+++ src/Table.js
@@ -10,42 +10,37 @@ function presentable(string) {
     return chopWithEllipses(string, MAX_STRING_LEN);
 }
 
-class Row extends React.Component {
-    render() {
-        const entry = this.props.entry;
-        return (
-            <tr>
-                <td>{presentable(String(entry[0]))}</td>
-                <td>{presentable(String(entry[1]))}</td>
-            </tr>
-        );
-    }
+function Row(props) {
+    const entry = props.entry;
+    return (
+        <tr>
+            <td>{presentable(String(entry[0]))}</td>
+            <td>{presentable(String(entry[1]))}</td>
+        </tr>
+    );
 }
 
-class TBody extends React.Component {
-    rows() {
-        return this.props.rows?.map(entry => <Row key={entry[0]} entry={entry} />);
+function TBody(props) {
+    function rows() {
+        return props.rows?.map(entry => <Row key={entry[0]} entry={entry} />);
     }
-    render() {
-        return (
-            <tbody>{ this.rows() }</tbody>
-        );
-    }
+
+    return (
+        <tbody>{ rows(props.rows) }</tbody>
+    );
 }
 
 export default
-class Table extends React.Component {
-    render() {
-        return (
-            <table>
-                <thead>
-                    <tr>
-                        <th className="table-title" colSpan="2">{this.props.title}</th>
-                    </tr>
-                </thead>
-                
-                <TBody rows={this.props.rows} />
-            </table>
-        );
-    }
+function Table(props) {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th className="table-title" colSpan="2">{props.title}</th>
+                </tr>
+            </thead>
+            
+            <TBody rows={props.rows} />
+        </table>
+    );
 }

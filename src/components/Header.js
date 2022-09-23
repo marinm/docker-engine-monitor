c1d4120747e2cb154d4d881f22f4cda8d0531d14
marinm <marinmema@live.ca>
2022-09-23 01:20:48 -0400

    Darken selected tab

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,18 +5,22 @@ function Header(props) {
 
     const {panelSwitch} = props;
 
-    const [, setPanelName] = panelSwitch;
+    const [panelName, setPanelName] = panelSwitch;
+
+    function selected(name) {
+        return (name === panelName) ? 'selected' : '';
+    }
 
     return (
         <div className="Header">
             <div className="layout-box">
                 <h1>Docker Engine Monitor</h1>
                 <ul className="tab-bar">
-                    <li onClick={() => setPanelName('engine')} >Engine</li>
-                    <li onClick={() => setPanelName('containers')} >Containers</li>
-                    <li onClick={() => setPanelName('networks')} >Networks</li>
-                    <li onClick={() => setPanelName('volumes')} >Volumes</li>
-                    <li onClick={() => setPanelName('images')} >Images</li>
+                    <li className={selected('engine')}      onClick={() => setPanelName('engine')} >Engine</li>
+                    <li className={selected('containers')}  onClick={() => setPanelName('containers')} >Containers</li>
+                    <li className={selected('networks')}    onClick={() => setPanelName('networks')} >Networks</li>
+                    <li className={selected('volumes')}     onClick={() => setPanelName('volumes')} >Volumes</li>
+                    <li className={selected('images')}      onClick={() => setPanelName('images')} >Images</li>
                 </ul>
             </div>
         </div>
